d4ccf7fcdac1b3b5aa8acfc0b4ce08e0d85daf78
Ritchie <74891400+SuperRitchie@users.noreply.github.com>
2022-03-28 01:39:49 -0700

    implement

--- demo/src/test/java/com/example/demo/Main.java
+++ demo/src/test/java/com/example/demo/Main.java
@@ -12,11 +12,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter username");
-
-        String userName = myObj.nextLine();  // Read user input
-        System.out.println("Username is: " + userName);  // Output user input
+        String username = System.getenv("USERNAME");
+        System.out.println(username);
         System.out.println(TOTPGenerator.getTwoFactorCode());
         //WebDriverManager.chromedriver().setup();
         System.setProperty("webdriver.chrome.driver", "D:\\a\\Automatic-U-Pass-BC-Renewer\\Automatic-U-Pass-BC-Renewer\\chromedriver.exe");
