997f0270e4e340249d49bbafdf93b49d09bf7544
Ritchie <flamener@hotmail.com>
2022-04-17 21:36:15 -0700

    update libraries and chromedriver

--- demo/src/test/java/com/example/demo/Main.java
+++ demo/src/test/java/com/example/demo/Main.java
@@ -7,16 +7,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.Scanner;
-//import io.github.bonigarcia.wdm.WebDriverManager;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Main {
     public static void main(String[] args) {
         String username = System.getenv("USERNAME");
         System.out.println(username);
-        System.out.println(TOTPGenerator.getTwoFactorCode());
-        //WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver", "D:\\a\\Automatic-U-Pass-BC-Renewer\\Automatic-U-Pass-BC-Renewer\\chromedriver.exe");
+        System.out.println(TOTPGenerator.getTwoFactorCode("test"));
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://upassbc.translink.ca/");
         driver.manage().window().maximize();
